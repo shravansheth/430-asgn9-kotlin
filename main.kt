71eee11278823cdbd072e229b3bf118c3c76b805
Tyler Le <tylerle122002@gmail.com>
2024-03-13 17:44:35 -0700

    added matching patern to interp

--- main.kt
+++ main.kt
@@ -1,3 +1,43 @@
+interface ExprC
+{
+
+}
+
+class NumC(val test: Int = 0) : ExprC
+{
+
+}
+
+class Env
+{
+
+}
+
+class Value 
+{
+
+}
+
+
+fun interp(exp: ExprC, env: Env) {
+  when {
+    exp is NumC && exp.test == 10 -> println("1 " + exp.test)
+    exp is NumC && exp.test == 13 -> println("2 " + exp.test)
+    else -> println("3")
+  }
+}
+
+fun parse(s: String) {
+
+}
+
+fun serialize(value: Value) {
+
+}
+
 fun main(args: Array<String>) {
   println("Hello World!")
+  
+  val obj = NumC(10)
+  interp(obj, Env())
 }
